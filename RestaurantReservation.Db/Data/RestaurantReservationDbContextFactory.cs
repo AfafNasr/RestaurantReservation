ea1d9075fbd1f64ff9f02faa2b929d2e04438715
Afaf Naser <afafnaser226@gmail.com>
2026-08-03 10:12:24 -0700

    feat: add initial database schema with EF Core migrations

---
+++ RestaurantReservation.Db/Data/RestaurantReservationDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace RestaurantReservation.Db.Data;
+
+public class RestaurantReservationDbContextFactory
+    : IDesignTimeDbContextFactory<RestaurantReservationDbContext>
+{
+    public RestaurantReservationDbContext CreateDbContext(string[] args)
+    {
+        DbContextOptions<RestaurantReservationDbContext> options =
+            new DbContextOptionsBuilder<RestaurantReservationDbContext>()
+                .UseSqlServer(
+                    "Server=.;Database=RestaurantReservationCore;Trusted_Connection=True;TrustServerCertificate=True;")
+                .Options;
+
+        return new RestaurantReservationDbContext(options);
+    }
+}
